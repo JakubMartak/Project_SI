7780b0b341e0ad9e835b33b9b4f11e5679915d05
Jakub Marták <jakubmartak@centrum.sk>
2022-11-11 11:56:51 +0100

    manuál query

--- ESPNFUVNM/src/app/Http/Controllers/TestController3.php
+++ ESPNFUVNM/src/app/Http/Controllers/TestController3.php
@@ -1,10 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
 
 class TestController3 extends Controller
 {
     public function test3(){
-        echo 24;
+        $users = DB::table('users')->get();
+
+        foreach ($users as $user) {
+            echo $user->name;
+        }
+
     }
 }
--- ESPNFUVNM/src/routes/web.php
+++ ESPNFUVNM/src/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\TestController3;
 use Illuminate\Support\Facades\Route;
 
 /*
