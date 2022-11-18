f3afe7eea616f0def048c16d4919b74fb883f5fa
Jakub Marták <jakubmartak@centrum.sk>
2022-11-18 18:59:44 +0100

    test commit

--- ESPNFUVNM/src/routes/web.php
+++ ESPNFUVNM/src/routes/web.php
@@ -36,9 +36,33 @@ Route::get('/createprax', [TestController::class, 'createpraxpage']);
 
 /* student routes */
 Route::get('/stuPracList', [TestController::class, 'stuPracList']);
+Route::get('/stuPracProgList', [TestController::class, 'stuPracProgList']);
+Route::get('/stuCompAdd', [TestController::class, 'stuCompAdd']);
+Route::get('/stuCompUpd', [TestController::class, 'stuCompUpd']);
+Route::get('/stuCompDel', [TestController::class, 'stuCompDel']);
+Route::get('/stuPersAdd', [TestController::class, 'stuPersAdd']);
+Route::get('/stuPersUpd', [TestController::class, 'TestController']);
+Route::get('/stuPersDel', [TestController::class, 'stuPersDel']);
+Route::get('/stuContrList', [TestController::class, 'stuContrList']);
+Route::get('/stuContrTypeUpd', [TestController::class, 'stuContrTypeUpd']);
+Route::get('/stuContrTypeList', [TestController::class, 'stuContrTypeList']);
+Route::get('/stuContrAdd', [TestController::class, 'stuContrAdd']);
+Route::get('/stuContrUpd', [TestController::class, 'stuContrUpd']);
+Route::get('/stuContrDel', [TestController::class, 'stuContrDel']);
+Route::get('/stuContReportList', [TestController::class, 'stuContReportList']);
+Route::get('/stuContReportAdd', [TestController::class, 'stuContReportAdd']);
+Route::get('/stuContReportUpd', [TestController::class, 'stuContReportUpd']);
+Route::get('/stuContractRepDel', [TestController::class, 'stuContractRepDel']);
+Route::get('/stuPracReportRead', [TestController::class, 'stuPracReportRead']);
+Route::get('/stuFeedRead', [TestController::class, 'stuFeedRead']);
+Route::get('/stuFeedAdd', [TestController::class, 'stuFeedAdd']);
+Route::get('/stuFeedUpd', [TestController::class, 'stuFeedUpd']);
+Route::get('/stuFeedDel', [TestController::class, 'stuFeedDel']);
+Route::get('/stuPracCertRead', [TestController::class, 'stuPracCertRead']);
 
 /* head of workplace routes */
 Route::get('/headCompList', [TestController::class, 'headCompList']);
+Route::get('/headCompAdd', [TestController::class, 'headCompAdd']);
 Route::get('/headStudList', [TestController::class, 'headStudList']);
 Route::get('/headPracList', [TestController::class, 'headPracList']);
 Route::get('/headPracFinishList', [TestController::class, 'headPracFinishList']);
@@ -49,6 +73,7 @@ Route::get('/headFeedRead', [TestController::class, 'headFeedRead']);
 Route::get('/headCertList', [TestController::class, 'headCertList']);
 Route::get('/headRespList', [TestController::class, 'headRespList']);
 Route::get('/headRespUpd', [TestController::class, 'headRespUpd']);
+Route::get('/headRespAdd', [TestController::class, 'headRespAdd']);
 
 /* responsible preson functions */
 Route::get('/respCompList', [TestController::class, 'respCompList']);
