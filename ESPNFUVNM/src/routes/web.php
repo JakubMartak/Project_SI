63783387de0604c2440444e86c429ecfcb5fcbd5
MarekHrabcak <marek.hrabcak@student.ukf.sk>
2022-11-23 18:04:06 +0100

    Dalsia neuspesna verzia insertu

--- ESPNFUVNM/src/routes/web.php
+++ ESPNFUVNM/src/routes/web.php
@@ -129,6 +129,13 @@ Route::get('/cmpFeedAdd', [TestController::class, 'cmpFeedAdd']);
 Route::get('/cmpFeedUpd', [TestController::class, 'cmpFeedUpd']);
 
 
+
+// Controller-name@method-name
+/*Route::get('/', 'PagesController@index'); // localhost:8000/
+Route::get('/{id}', 'PagesController@index');
+Route::post('/save', 'PagesController@save');
+Route::get('/deleteUser/{id}', 'PagesController@deleteUser');*/
+
 require __DIR__.'/auth.php';
 
 
