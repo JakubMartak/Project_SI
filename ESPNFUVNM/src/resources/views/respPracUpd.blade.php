e39b569ab466fba367f90d1996dae3c9ea7f4bc2
MartinKratky <64469323+Matko009@users.noreply.github.com>
2022-11-29 18:06:22 +0100

    resp crud

--- ESPNFUVNM/src/resources/views/respPracUpd.blade.php
+++ ESPNFUVNM/src/resources/views/respPracUpd.blade.php
@@ -98,13 +98,13 @@
                                     echo"</select></td>
             <td>
                 <div class='d-flex mb-3'>
-                    <button type='submit' class='btn btn-primary'>Confirm</button>
+                    <button type='submit' class='btn btn-primary'>Potvrdiť</button>
                 </div>
             </td>
 
             <td>
                 <div class='d-flex mb-3'>
-                    <a class='btn btn-danger' href='/respPracRead'>Cancel</a>
+                    <a class='btn btn-danger' href='/respPracRead'>Zrušiť</a>
                 </div>
             </td>"
                                     ?>
