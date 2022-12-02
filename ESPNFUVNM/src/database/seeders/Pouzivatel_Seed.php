92a6889d03ed18f429b4d8e24f6d12b1b7a7002b
MartinKratky <64469323+Matko009@users.noreply.github.com>
2022-12-02 18:06:16 +0100

    Ďalšie seeds

--- ESPNFUVNM/src/database/seeders/Pouzivatel_Seed.php
+++ ESPNFUVNM/src/database/seeders/Pouzivatel_Seed.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\DB;
 class Pouzivatel_Seed extends Seeder
 {
     public function run(){
+        //id1
         DB::table('Pouzivatel')->insert([
             'Meno'=> 'Martin',
             'Priezvisko' => 'Krátky',
@@ -15,6 +16,7 @@ class Pouzivatel_Seed extends Seeder
             'Mail' => 'martin.kratky@student.ukf.sk',
             'Rola_pouzivatela' => 1,
         ]);
+        //id2
         DB::table('Pouzivatel')->insert([
             'Meno'=> 'Jakub',
             'Priezvisko' => 'Martak',
@@ -22,17 +24,66 @@ class Pouzivatel_Seed extends Seeder
             'Mail' => 'jakub.martak@student.ukf.sk',
             'Rola_pouzivatela' => 1,
         ]);
+        //id3
         DB::table('Pouzivatel')->insert([
             'Meno'=> 'Peter',
             'Priezvisko' => 'Dobrý',
             'Mail' => 'peter@ukf.sk',
             'Rola_pouzivatela' => 2,
         ]);
+        //id4
         DB::table('Pouzivatel')->insert([
             'Meno'=> 'Adam',
             'Priezvisko' => 'Zlý',
             'Mail' => 'adam973@gmail.com',
             'Rola_pouzivatela' => 3,
+            'Firma_idFirma' => 1,
+        ]);
+        //id5
+        DB::table('Pouzivatel')->insert([
+            'Meno'=> 'admin',
+            'Priezvisko' => 'admin',
+            'Mail' => 'admin@ukf.sk.com',
+            'Rola_pouzivatela' => 5,
+        ]);
+        //id6
+        DB::table('Pouzivatel')->insert([
+            'Meno'=> 'Peter',
+            'Priezvisko' => 'Druhý',
+            'Mail' => 'peter.druhy@gmail.com',
+            'Rola_pouzivatela' => 3,
+            'Firma_idFirma' => 2,
+        ]);
+        //id7
+        DB::table('Pouzivatel')->insert([
+            'Meno'=> 'Marcel',
+            'Priezvisko' => 'Tretí',
+            'Mail' => 'marcel.treti@gmail.com',
+            'Rola_pouzivatela' => 3,
+            'Firma_idFirma' => 3,
+        ]);
+        //id8
+        DB::table('Pouzivatel')->insert([
+            'Meno'=> 'Ivan',
+            'Priezvisko' => 'Hrozný',
+            'Mail' => 'ivan.hrozny@gmail.com',
+            'Rola_pouzivatela' => 3,
+            'Firma_idFirma' => 4,
+        ]);
+        //id9
+        DB::table('Pouzivatel')->insert([
+            'Meno'=> 'Jozef',
+            'Priezvisko' => 'Kral',
+            'Mail' => 'jozef.kral@gmail.com',
+            'Rola_pouzivatela' => 3,
+            'Firma_idFirma' => 5,
+        ]);
+        //id10
+        DB::table('Pouzivatel')->insert([
+            'Meno'=> 'Jan',
+            'Priezvisko' => 'Veduci',
+            'Mail' => 'jan.veduci@gmail.com',
+            'Rola_pouzivatela' => 4,
         ]);
     }
 }
